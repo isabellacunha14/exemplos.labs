02c64de4952c5cbaadfc17b1ca8dcdaaf9fa6a88
Isabella <isa.bela.cunha@hotmail.com>
2025-04-06 14:15:25 -0300

    lab01

---
+++ lab01.py
@@ -0,0 +1,15 @@
+# Leitura dos dois números inteiros
+a = int(input())
+b = int(input())
+
+# Exibição dos valores armazenados
+print("a:", a)
+print("b:", b)
+
+# Operações aritméticas e seus resultados
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Exponenciação:", a ** b)
+print("Divisão Inteira:", a // b)
+print("Resto da Divisão:", a % b)
